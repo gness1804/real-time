5841358b4d68a57c72bfecc795024628fe508bb6
gness1804 <graham.nessler@gmail.com>
2017-02-01 15:33:48 -0700

    First real test passing

--- test/test.js
+++ test/test.js
@@ -1,24 +1,22 @@
 'use strict';
+var chai = require('chai');
 const assert = require('assert');
 const request = require('supertest');
 const express = require('express');
 const app = express();
 
-const chai = require('chai');
+const expect = chai.expect();
 const should = chai.should();
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-describe('dummy test', () => {
-  it('tests should work', () => {
-
-    assert(true)
-  });
-});
-
-describe('GET', () => {
-  it('', () => {
-
-
+describe('GET /', () => {
+  it('should return a 200 status message', () => {
+    request(app)
+      .get('/')
+      .end(function(error, result) {
+        res.should.have.status(2000);
+        done();
+      })
   });
 });
