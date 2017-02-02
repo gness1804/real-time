7530832840618fe7c2737ad84b35fd9cdf547f89
gness1804 <graham.nessler@gmail.com>
2017-02-02 12:18:10 -0700

    Add passing test for submit button and no input text

--- test/feature-tests.js
+++ test/feature-tests.js
@@ -22,17 +22,37 @@ test.describe('Input fields', () => {
 
   test.it('input fields should render with correct text', () => {
 
-    driver.findElement({id: 'question-title-input'}).then((button) => {
-     return button.getAttribute('placeholder')
+    driver.findElement({id: 'question-title-input'}).then((field) => {
+     return field.getAttribute('placeholder')
    }).then((text) => {
      assert.strictEqual(text, 'Question Title');
    })
 
-   driver.findElement({id: 'second-choice-input'}).then((button) => {
-    return button.getAttribute('placeholder')
+   driver.findElement({id: 'second-choice-input'}).then((field) => {
+    return field.getAttribute('placeholder')
   }).then((text) => {
     assert.strictEqual(text, 'Second Choice');
   })
+
+  driver.findElement({id: 'fourth-choice-input'}).then((field) => {
+   return field.getAttribute('placeholder')
+ }).then((text) => {
+   assert.strictEqual(text, 'Fourth Choice');
+ })
+
+  });
+
+  test.it('submit button should trigger error message if one or more input fields is empty', () => {
+
+    const submitButton = driver.findElement({id: 'submit-question'});
+    submitButton.click();
+
+    driver.findElement({id: 'user-notification'}).then((button) => {
+     return button.getText()
+   }).then((text) => {
+     assert.strictEqual(text, 'Please enter in valid values for all fields.');
+   })
+
 
   });
 });
