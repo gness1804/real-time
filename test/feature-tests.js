60c27cd1cc36f3578ae61855cfe45d4aef551e03
gness1804 <graham.nessler@gmail.com>
2017-02-02 12:28:57 -0700

    Change ES6 arrow functions to ES5 functions in test to deal with scoping issue

--- test/feature-tests.js
+++ test/feature-tests.js
@@ -5,54 +5,55 @@ const driver = new webdriver.Builder()
   .forBrowser('chrome')
   .build();
 
-test.describe('Input fields', () => {
+test.describe('Input fields', function () {
+  this.timeout(15000);
 
-  test.beforeEach(() => {
+  test.beforeEach(function() {
       driver.get('http://localhost:3000');
     });
 
-  test.it('application should render five input fields on landing page', () => {
-    // this.timeout(15000);
+  test.it('application should render five input fields on landing page', function () {
 
-    const inputFields = driver.findElements({tagName: 'input'}).then((select) => {
+    const inputFields = driver.findElements({tagName: 'input'}).then(function(select) {
       assert.equal(select.length, 5)
     })
 
   });
 
-  test.it('input fields should render with correct text', () => {
+  test.it('input fields should render with correct text', function () {
 
-    driver.findElement({id: 'question-title-input'}).then((field) => {
+    driver.findElement({id: 'question-title-input'}).then(function (field) {
      return field.getAttribute('placeholder')
-   }).then((text) => {
+   }).then(function (text) {
      assert.strictEqual(text, 'Question Title');
    })
 
-   driver.findElement({id: 'second-choice-input'}).then((field) => {
+   driver.findElement({id: 'second-choice-input'}).then(function (field){
     return field.getAttribute('placeholder')
-  }).then((text) => {
+  }).then(function (text){
     assert.strictEqual(text, 'Second Choice');
   })
 
-  driver.findElement({id: 'fourth-choice-input'}).then((field) => {
+  driver.findElement({id: 'fourth-choice-input'}).then(function (field) {
    return field.getAttribute('placeholder')
- }).then((text) => {
+ }).then(function (text){
    assert.strictEqual(text, 'Fourth Choice');
  })
 
   });
 
-  test.it('submit button should trigger error message if one or more input fields is empty', () => {
+  test.it('submit button should trigger error message if one or more input fields is empty', function () {
 
     const submitButton = driver.findElement({id: 'submit-question'});
     submitButton.click();
 
-    driver.findElement({id: 'user-notification'}).then((button) => {
+    driver.findElement({id: 'user-notification'}).then(function (button) {
      return button.getText()
-   }).then((text) => {
+   }).then(function (text) {
      assert.strictEqual(text, 'Please enter in valid values for all fields.');
    })
 
+   //add info in one or two input fields but not all; submit should still fail
 
   });
 });
